fadfaa676b409ee60a8c2dd5c89316c2611d8871
Hinata10 <shinkai.xzettai.62@icloud.com>
2019-07-10 17:34:22 +0900

    Initial Commit

---
+++ FlappyBird/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlappyBird
+//
+//  Created by 日向亮博 on 2019/07/10.
+//  Copyright © 2019 Hinata10. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
